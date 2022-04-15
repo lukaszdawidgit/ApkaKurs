c83a06f9d9854d984c850f7a9e18ffec948bf64c
lukas <lukasz.dawid@hotmail.com>
2022-04-15 10:55:05 +0200

    Add project files.

---
+++ P02PrzykladKomunikacjiPomiedzyKlasami/Osoba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02PrzykladKomunikacjiPomiedzyKlasami
+{
+    //https://github.com/tomasz-trener/06WDzien7
+    class Osoba
+    {
+        string imie;
+        string nazwisko;
+
+        public string ImieNazwisko
+        {
+            get { return imie + " " + nazwisko; } 
+        }
+
+        Samochod samochod;
+
+        public void KupSamochod()
+        {
+            samochod = new Samochod(this);
+        }
+
+        public void PochwalSieSamochodem()
+        {
+            Console.WriteLine("Kolor mojego " +
+                "samochodu to: " + samochod.Kolor);
+        }
+
+    }
+}
